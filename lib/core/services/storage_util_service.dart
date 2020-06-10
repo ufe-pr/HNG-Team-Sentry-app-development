d680d894b1bcedd97db5601dbbf48c811d9745fa
Mamus Eferha <eferhamamus@yahoo.com>
2020-06-11 00:14:01 +0100

    feat:storage_util_service

---
+++ lib/core/services/storage_util_service.dart
@@ -0,0 +1,63 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageUtil {
+  static StorageUtil _storageUtil;
+  static SharedPreferences _preferences;
+
+  static Future getInstance() async {
+    if (_storageUtil == null) {
+
+      // keep local instance till it is fully initialized.
+
+      var secureStorage = StorageUtil._();
+      await secureStorage._init();
+      _storageUtil = secureStorage;
+    }
+    return _storageUtil;
+  }
+  StorageUtil._();
+  Future _init() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+  // get string
+  static String getString(String key, {String defValue = ''}) {
+    if (_preferences == null) return defValue;
+    return _preferences.getString(key) ?? defValue;
+  }
+  // set string
+  static Future putString(String key, String value) {
+    if (_preferences == null) return null;
+    return _preferences.setString(key, value);
+  }
+  // get int
+   static int getInt(String key, {int defValue = 0}) {
+    if (_preferences == null) return defValue;
+    return _preferences.getInt(key) ?? defValue;
+  }
+  // set int
+  static Future putInt(String key, int value) {
+    if (_preferences == null) return null;
+    return _preferences.setInt(key, value);
+  }
+  //set bool
+   static bool getBool(String key, {bool defValue = false}) {
+    if (_preferences == null) return defValue;
+    return _preferences.getBool(key) ?? defValue;
+  }
+  //get bool
+  static Future putBool(String key, bool value) {
+    if (_preferences == null) return null;
+    return _preferences.setBool(key, value);
+  }
+
+//set List
+static List getStringList(String key,) {
+    if (_preferences == null) return [];
+    return _preferences.getStringList(key) ?? [];
+  }
+  //get List
+  static Future putStringList(String key, List value) {
+    if (_preferences == null) return null;
+    return _preferences.setStringList(key, value);
+  }
+}
